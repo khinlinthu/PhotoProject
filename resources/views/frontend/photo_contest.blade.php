919971ebf745028f5bc0d3b4ee43852a1cbd5460
ThuZarAung <thuzar351996@gmail.com>
2021-01-12 09:42:58 +0630

    Update UI

--- resources/views/frontend/photo_contest.blade.php
+++ resources/views/frontend/photo_contest.blade.php
@@ -6,8 +6,9 @@
         <div class="slider-area">
             <div class="bend niceties preview-2">
                 <div id="ensign-nivoslider" class="slides">
-                    <img src="{{asset('frontend_asset/images/scw.png')}}" alt="" title="#slider-direction-1" />
-                    <img src="{{asset('frontend_asset/images/scw.png')}}" alt="" title="#slider-direction-2" />
+                    <img src="{{asset('frontend_asset/images/1.jpg')}}" alt="" title="#slider-direction-1" />
+                    <img src="{{asset('frontend_asset/images/2.jpg')}}" alt="" title="#slider-direction-2" />
+                    <img src="{{asset('frontend_asset/images/3.jpg')}}" alt="" title="#slider-direction-3" />
                 </div>
                 <!-- direction 2 -->
                 {{-- <div id="slider-direction-1" class="slider-direction">
@@ -154,5 +155,103 @@
             </div>
         </div>
         <!-- Partner Logo Area End Here -->
+
+@endsection
+
+@section('content1')
+
+<!-- Footer Area Section Start Here -->
+<footer>
+    <div class="footer-top-area pt-100 pb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-sm-12">
+                    <div class="single-footer footer-one">
+                        <h3>About Company</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat, maxime vel alias impedit veritatis temporibus, sequi quos veniam eius optio corporis modi dicta molestias at inventore culpa, natus explicabo.</p>
+                        <div class="footer-social-media-area">
+                            <nav>
+                                <ul>
+                                    <li><a href="https://www.facebook.com/scwtravel"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="https://www.instagram.com/scw_travel/?fbclid=IwAR3NGM9ACOnGq0-Al3EM-yL77282rMM8ucBb1jPw7rkNZD9-ccxhAILX3dw"><i class="fa fa-instagram"></i></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 hidden-sm">
+                    <div class="single-footer footer-two">
+                        <h3>Twitter Feed</h3>
+                        <nav>
+                            <ul>
+                                <li><i class="fa fa-twitter"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">https://twitter.com/?lang=en</a>
+                                    <br/> 3 days ago
+                                </li>
+                                <li><i class="fa fa-twitter"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">https://twitter.com/?lang=en</a>
+                                    <br/> 3 days ago
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-3 hidden-md col-sm-12">
+                    <div class="single-footer footer-three">
+                        <h3>Sponsorship</h3>
+                        <ul>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/AYA Bank.jpg')}}" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/CB bank.png')}}" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/MCB Bank.jpg')}}" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/myanmar_tourism_bank.jpg')}}" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/Myanmr Apex Bank.png')}}" alt="flicker photo"></a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="{{asset('frontend_asset/sponsorimages/Shwe Bank.jpg')}}" alt="flicker photo"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-12">
+                    <div class="single-footer footer-four margin-0">
+                        <h3>Corporate Office</h3>
+                        <nav>
+                            <ul>
+                                <li><i class="fa fa-paper-plane-o"></i> No.25, Ivory Court Villa Pun Hlaing Goft Estate, Haling Thar Yar Tsp, Yangon. (Head Office) </li>
+                                <li><i class="fa fa-paper-plane-o"></i> No.22/B. 1st Floor, Thiri Myaing 4th Street, Hlaing Tsp, Yangon. (Branch Office) </li>
+                                <li><i class="fa fa-phone"></i> <a href="tel:+959-899 3333 87">+959-799 3333 87</a></li>
+                                <li><i class="fa fa-envelope-o"></i> <a href="mailto:yourmail@gmail.com">zinlinn@scwmyanmar.com.mm</a></li>
+                                {{-- <li><i class="fa fa-fax"></i> Fax: (123) 4589761</li> --}}
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="footer-bottom">
+                        <p> &copy; 2021 SCW Myanmar CO., LTD. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Area Section End Here -->
 
 @endsection
